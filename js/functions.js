b1a97580cd80f75e97eca44e8a4bf7612c3f5770
Marcin <marcin@dobrzanski.me>
2022-11-08 13:48:22 +0100

    Add reading computer movement to script.js

--- js/functions.js
+++ js/functions.js
@@ -11,3 +11,7 @@ function clearMessages(){
 let computerMove = `kamień`;
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + randomNumber);
