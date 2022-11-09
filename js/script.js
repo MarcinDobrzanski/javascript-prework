214610bad1b73a20df73b9d04f04f272b0023e85
Marcin <marcin@dobrzanski.me>
2022-11-09 11:02:54 +0100

    Fix playerInput and optymize game result

--- js/script.js
+++ js/script.js
@@ -23,31 +23,21 @@ let playerMove = 'nieznany ruch';
 if (playerInput == '1') {
 	playerMove = 'kamień';
 } else if (playerInput == '2') {
-	computerMove = 'papier';
+	playerMove = 'papier';
 } else if (playerInput == '3') {
-	computerMove = 'nożyce';
+	playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if (computerMove == 'kamień' && playerMove == 'papier') {
+if (
+	(computerMove == 'kamień' && playerMove == 'papier') ||
+	(computerMove == 'nożyce' && playerMove == 'kamień') ||
+	(computerMove == 'papier' && playerMove == 'nożyce')
+) {
 	printMessage('Ty wygrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-	printMessage('Ty wygrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-	printMessage('Ty wygrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-	printMessage('Ty przegrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'kamień') {
-	printMessage('Ty przegrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'papier') {
-	printMessage('Ty przegrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-	printMessage('Remis!');
-} else if (computerMove == 'papier' && playerMove == 'papier') {
-	printMessage('Remis!');
-} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+} else if (computerMove == playerMove) {
 	printMessage('Remis!');
 } else {
-	printMessage('nieznany ruch');
+	printMessage('Ty przegrywasz!');
 }
