fd6f1f84bde101f1fbdabacea7aa73b0cb284e3a
Marcin <marcin@dobrzanski.me>
2022-11-09 13:27:10 +0100

    Add function displayResult

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
+function getMoveName(argMoveId) {
+	if (argMoveId == 1) {
+		return 'kamień';
+	} else if (argMoveId == 2) {
+		return 'papier';
+	} else if (argMoveId == 3) {
+		return 'nożyce';
+	} else {
+		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+		return 'nieznany ruch';
+	}
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+	console.log('moves:', argComputerMove, argPlayerMove);
+	if (
+		(argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+		(argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
+		(argComputerMove == 'papier' && argPlayerMove == 'nożyce')
+	) {
+		printMessage('Ty wygrywasz!');
+		return 'Ty wygrywasz!';
+	} else if (argComputerMove == argPlayerMove) {
+		printMessage('Tym razem remis!');
+		return 'Tym razem remis!';
+	} else {
+		printMessage('Tym razem przegrywasz :(');
+		return 'Tym razem przegrywasz :(';
+	}
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
+/*
 if (randomNumber == 1) {
 	computerMove = 'kamień';
 } else if (randomNumber == 2) {
@@ -13,13 +45,18 @@ if (randomNumber == 1) {
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+*/
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
+
+let wonMove = displayResult(computerMove, playerMove);
+
+/*
 if (playerInput == '1') {
 	playerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -28,7 +65,9 @@ if (playerInput == '1') {
 	playerMove = 'nożyce';
 }
 
+
 printMessage('Twój ruch to: ' + playerMove);
+
 
 if (
 	(computerMove == 'kamień' && playerMove == 'papier') ||
@@ -41,3 +80,4 @@ if (
 } else {
 	printMessage('Ty przegrywasz!');
 }
+*/
